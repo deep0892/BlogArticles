958526449e509de129ac15548962b254b16e13c6
Deepankar <deepankar080892@gmail.com>
2020-06-25 20:20:58 +0200

    Setting routes for creating and editing blogs

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
 const articleRoute = require('./routes/articles');
+const Article = require('./models/article');
 
 mongoose.connect('mongodb://localhost:27017/blog', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.use('/articles', articleRoute);
 
-app.get('/', (req, res) => {
-    const articles = [{
-            title: 'Test Article',
-            description: 'Test description',
-            createdAt: new Date()
-        },
-        {
-            title: 'Test Article 2',
-            description: 'Test description 2',
-            createdAt: new Date()
-        }
-    ];
+app.get('/', async(req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' });
     res.render('articles/index', { articles: articles });
 });
 
